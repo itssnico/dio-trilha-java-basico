f9ffc0d21e4f594c4ad61e3563cf34a1dab5c33a
itssnico <nicolaspf1@hotmail.com>
2023-08-31 18:03:30 -0300

    variavel-constante

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // defindo somente a variavel faz com que o valor dela possa mudar
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        // o final transforma a variavel em uma constante.
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+
     }
 }
